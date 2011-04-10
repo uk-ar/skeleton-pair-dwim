9d06acd6f25cfec62893ba268ad85e78eeed6c9f
yuuki arisawa <yuuki.ari@gmail.com>
2011-04-11 07:22:35 +0900

    can modify condition and function

--- skeleton-pair-dwim.el
+++ skeleton-pair-dwim.el
@@ -20,6 +20,13 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;; It can be useful to insert parentheses, braces, quotes and the like in matching pairs – e.g., pressing “(” inserts ‘()’, with the cursor in between
+
+;; (auto-install-from-url "https://github.com/uk-ar/skeleton-pair-dwim/raw/master/skeleton-pair-dwim.el")
+
+;;; Bug:
+;; cannot move over `'
+
 
 ;;(setq skeleton-pair-alist '((?| _ ?|) ) )
 
@@ -30,8 +37,9 @@
 
 (progn
   ;;setting for skeleton.el
-
+  (setq skeleton-pair-on-word nil)
   (setq skeleton-pair t)
+
   ;; not to deactivate mark
   (defadvice skeleton-insert
     (around skeleton-insert-dwim-advice)
@@ -45,84 +53,113 @@
 	  (set-mark (1+(mark)))))))
   )
 
-(defun skeleton-pair-dwim-closep (char)
-  (let ((skeleton
-	 (or (rassoc `(_ ,char) skeleton-pair-alist)
-	     (rassoc `(_ ,char) skeleton-pair-default-alist))))
-    (if (and skeleton (eq (car skeleton) (nth 2 skeleton)))
-	nil
-      skeleton)))
+(progn
+  ;;condition
+  (defun skeleton-pair-dwim-closep (char)
+    (let ((skeleton
+	   (or (rassoc `(_ ,char) skeleton-pair-alist)
+	       (rassoc `(_ ,char) skeleton-pair-default-alist))))
+      (if (and skeleton (eq (car skeleton) (nth 2 skeleton)))
+	  nil
+	skeleton)))
 
-(defun skeleton-pair-dwim-openp (char)
-  (and
-   (not
-    ;; support for (?( _ ?)) (?\)) type alist(skeleton-pair-default-alist)
-    (skeleton-pair-dwim-closep char))
-   (let ((skeleton
-	  (or (assq char skeleton-pair-alist)
-	      (assq char skeleton-pair-default-alist))))
-     (if (and skeleton (eq (car skeleton) (nth 2 skeleton)))
-	 nil
-       skeleton))))
+  (defun skeleton-pair-dwim-openp (char)
+    (and
+     (not
+      ;; support for (?( _ ?)) (?\)) type alist(skeleton-pair-default-alist)
+      (skeleton-pair-dwim-closep char))
+     (let ((skeleton
+	    (or (assq char skeleton-pair-alist)
+		(assq char skeleton-pair-default-alist))))
+       (if (and skeleton (eq (car skeleton) (nth 2 skeleton)))
+	   nil
+	 skeleton))))
+  )
 
 ;;(defvar skeleton-pair-dwim-earmuff-only nil)
 ;;(setq skeleton-pair-dwim-earmuff-only t)
 ;;(setq skeleton-pair-dwim-earmuff-only nil)
 ;;interactive function
 ;;ok "" "aaa" over "aa aaa"
-(global-set-key (kbd "+") 'skeleton-pair-insert-dwim-earmuff)
-
-(defun skeleton-pair-insert-dwim-earmuff (arg)
-  (interactive "*p")
-  (skeleton-pair-insert-dwim arg t)
+;;(global-set-key (kbd "+") 'skeleton-pair-insert-dwim-earmuff)
+;;(global-set-key (kbd "+") 'self-insert-command)
+;;bug simple-hatena-mode)))
+(progn
+  ;;command
+  (defun skeleton-pair-insert-dwim-earmuff (arg)
+    (interactive "*p")
+    (skeleton-pair-insert-dwim arg t))
   )
 
-(defun skeleton-pair-insert-dwim (arg &optional earmuff-only)
-  (interactive "*p")
-  (let* ((char last-command-event)
-	 (closep (skeleton-pair-dwim-closep char))
-	 (openp (skeleton-pair-dwim-openp char))
-	 (mark (and skeleton-autowrap
-		    (or (eq last-command 'mouse-drag-region)
-			(and transient-mark-mode mark-active))))
-	 (can-earmuff (and earmuff-only mark))
-	 (skeleton-end-hook))
-    ;;(message "%s %s %s %s" skeleton-pair-dwim-earmuff-only  (not can-earmuff) mark skeleton-pair-dwim-earmuff-only)
-    (if (or (not skeleton-pair)
-	    (and earmuff-only
-		(not can-earmuff)))
-	(progn
-	  ;;(message "m0")
-	  (self-insert-command (prefix-numeric-value arg))
-	)
+(labels ((dmessage (string &rest arg)
+		   (apply 'message string arg)
+		   ))
+
+  (defun skeleton-pair-insert-dwim (arg &optional earmuff-only)
+    (interactive "*p")
+    (let* ((char last-command-event)
+	   (closep (skeleton-pair-dwim-closep char))
+	   (openp (skeleton-pair-dwim-openp char))
+	   (mark (and skeleton-autowrap
+		      (or (eq last-command 'mouse-drag-region)
+			  (and transient-mark-mode mark-active))))
+	   (can-earmuff (and earmuff-only mark))
+	   (skeleton (or closep openp `(,char _ ,char)))
+	   ;;(skeleton-end-hook)
+	   )
+      (if (or (not skeleton-pair)
+	      (and earmuff-only
+		   (not can-earmuff)))
+	  (progn
+	    (dmessage "m0")
+	    (self-insert-command (prefix-numeric-value arg))
+	    )
 	(cond (openp
 	       (skeleton-pair-insert-maybe nil);;40(
 	       (if (and mark (< (mark) (point)))
-		 (exchange-point-and-mark))
-	       ;;(message "m1")
+		   (exchange-point-and-mark))
+	       (dmessage "m1")
 	       )
 	      ((and (not mark)
 		    (eq (char-after) char)
 		    (eq this-command real-last-command));;41) & 34"
-	       (forward-char 1)
-	       ;;(message "m4")
+	       (forward-char 1);;move over close
+	       (dmessage "m4")
 	       )
 	      ((not closep)
 	       (skeleton-pair-insert-maybe nil);;34"
-	       ;;(message "m2")
+	       (dmessage "m2");; open and region
 	       )
 	      ((and mark closep)
 	       (skeleton-insert (cons nil closep) (if mark -1));;41)
 	       (if (and mark (< (point) (mark)))
-		 (exchange-point-and-mark))
-	       ;;(message "m3")
+		   (exchange-point-and-mark))
+	       (dmessage "m3");;region
 	       )
 	      (t ;;(not mark closep)
 	       (self-insert-command (prefix-numeric-value arg));;41)
-	       ;;(message "m5")
+	       (dmessage "m5")
 	       )
 	      ))))
+  )
 
+;;define-key
+;;insert pair t, self insert nil
+;;default self insert
+
+;;   "*括弧を挿入するときの関数一覧。
+;; 各要素は以下:
+;; \(CONDITION . FUNCTION)
+;; CONDITION を評価した結果、最初に真となる要素の FUNCTION が実行される。
+;; 真となる要素が存在しない場合 `self-insert-command' が実行される。")
+;; 
+;; (global-set-key (kbd "(") '(lambda (arg)
+;; 			       (interactive "p")
+;; 			       (message "%s" arg)))
+"aaa"
+(aaa abbb ddd   )
+(global-set-key (kbd "(") 'hoge)
+(global-set-key (kbd ")") 'hoge)
 (progn
   ;; filter functions for skeleton-pair-filter-function
   (defun skeleton-pair-dwim-inside-stringp ()
@@ -137,10 +174,95 @@
     (if (not (skeleton-pair-dwim-openp last-command-event))
 	(= last-command-event (preceding-char))))
   )
+(setq acp-insertion-functions
+      ;;(defvar acp-insertion-functions
+      '(;; (mark-active . (lambda(arg)
+	;; 		 (self-insert-command arg)
+	;; 		 (message "mark")))
+	(markp
+	 . (progn (skeleton-insert
+		   (cons nil skeleton) (if markp -1))
+		  (cond ((and openp (< (mark) (point)))
+			 (exchange-point-and-mark))
+			((and closep (< (point) (mark)))
+			 (exchange-point-and-mark)))
+		  (message "n1")))
+	((or
+	  ;;after escape
+	  (memq (char-syntax (preceding-char)) '(?\\ ?/))
+	  ;;before word
+	  (and (not closep) (looking-at "\\w"))
+	  ;;inside comment
+	  (not (memq (get-text-property (point) 'face)
+		     '(font-lock-comment-face font-lock-doc-face
+					      font-lock-string-face)))
+	  ;;after word
+	  ;;(and (not openp) (= (char-syntax (preceding-char)) ?w))
+	  )
+	 . (progn (self-insert-command arg)
+		  (message "n6fil")))
+	((and openp (memq (char-syntax (char-after)) '(?\\ ?/)))
+	      ()) .)
+	((and (not openp)
+	      (eq (char-after) char)
+	      (eq this-command real-last-command));;41) & 34"
+	 . (progn (forward-char 1);;move over close
+		  (message "n4")))
+	((not closep);;34" & 40(
+	 . (progn (skeleton-insert
+		   (cons nil skeleton) (if markp -1))
+		  ;;(skeleton-pair-insert-maybe nil)
+		  (message "n2");; open and region
+		 ))
+	;;(t . t)
+	;; (t . (lambda()
+	;; 	 (interactive)
+	;; 	 (self-insert-command arg)
+	;; 	 (message "default")))
+	(t . (progn (self-insert-command arg)
+		    (message "n5")))
+	;; (t . (lambda(arg)
+	;; 	      (interactive "p")
+	;; 	      (self-insert-command arg)
+	;; 	      (message "default c %s" arg)))
+	)
+      )
+
+(labels ((dmessage (string &rest arg)
+		   (apply 'message string arg)
+		   ))
+
+  (defun hoge (arg)
+    (interactive "*p")
+    (let* ((char last-command-event)
+	   (closep (skeleton-pair-dwim-closep char))
+	   (openp (skeleton-pair-dwim-openp char))
+	   (markp ;;(and skeleton-autowrap
+	    (or (eq last-command 'mouse-drag-region)
+		(and transient-mark-mode mark-active)))
+	   (skeleton-end-hook);;no to add new line
+	   ;;(can-earmuff (and earmuff-only mark))
+	   (skeleton (or closep openp `(,char _ ,char)))
+	   ;;(skeleton-end-hook)
+	   )
+      (catch 'break
+	(mapc (lambda (x)
+		(when (eval (car x))
+		  ;;(funcall (cdr x) n)
+		  ;;(funcall (cdr x) arg)
+		  (eval (cdr x))
+		  ;;(command-execute (cdr x))
+		  (throw 'break t)))
+	      (append acp-insertion-functions
+		      '((t . self-insert-command)))
+	      )
+	)
+      ))
+  )
 
 (progn
   ;; key binding utility
-  (defun skeleton-pair-dwim-default-alist-to-keys()
+  (defun skeleton-pair-dwim-default-pair-to-keys()
     (mapcar 'char-to-string
 	    (append (mapcar 'car
 			    skeleton-pair-alist)
@@ -161,7 +283,7 @@
 			   (char-to-string(nth 2 openp))))
 		     )keys)))
     )
-
+  
   (defun skeleton-pair-dwim-local-set-key(keys &optional command)
     (if (not (listp keys))
 	(setq keys (list keys)))
@@ -171,14 +293,6 @@
 		      (or command 'skeleton-pair-insert-dwim)))
      (skeleton-pair-dwim-parse-key keys)))
 
-  (defun skeleton-pair-dwim-global-set-key(keys &optional command)
-    (if (not (listp keys))
-	(setq keys (list keys)))
-    (mapc
-     (lambda (key)
-       (global-set-key key
-		       (or command 'skeleton-pair-insert-dwim)))
-     (skeleton-pair-dwim-parse-key keys)))
 
   (defun skeleton-pair-dwim-define-key(maps keys &optional command)
     (if (not (listp keys))
@@ -196,9 +310,26 @@
 	  )(skeleton-pair-dwim-parse-key keys))
        )maps))
   )
+(defun skeleton-pair-dwim-global-set-key(keys &optional command)
+  (if (not (listp keys))
+      (setq keys (list keys)))
+  (mapc
+   (lambda (key)
+     (global-set-key key
+		     (or command 'skeleton-pair-insert-dwim)))
+   (skeleton-pair-dwim-parse-key keys)))
+
+;;(global-set-key (kbd "\'") nil)
+;; (string-to-char "'")
+;; (global-set-key (char-to-string 39) nil)
+;; (global-set-key (char-to-string 40) ')
+;; (string-to-char "(")
+;; (string-to-char ")")
+
+;;(skeleton-pair-dwim-parse-key '("'" "\""))
+;;(skeleton-pair-dwim-global-set-key "'")
 ;;user-setting
-;;(skeleton-pair-dwim-define-key
- ;;'(global-map emacs-lisp-mode-map) '("{" "(" "\"" "'"))
+;;
 ;;global-map '("{" "(" "\"" "'"))
  ;;global-map '("{" "}" "(" ")" ))
  ;;'(global-map emacs-lisp-mode-map) ")")
@@ -208,12 +339,11 @@
 ;;(define-key global-map "(" 'skeleton-pair-insert-dwim)
 
 ;; region
-
+(defvar skeleton-pair-dwim-default-keys '("{" "(" "\"" "'" "["))
 (defun skeleton-pair-dwim-load-default ()
-  ;;defalut setting
+
   (ad-activate-regexp 'skeleton-insert-dwim-advice)
 
-  ;;default setting
   (setq skeleton-pair-filter-function
 	'(lambda()
 	   (or (skeleton-pair-dwim-inside-stringp)
@@ -221,11 +351,18 @@
 	       (skeleton-pair-dwim-after-same-as-charp)
 	       )))
 
-  ;; default
-  (skeleton-pair-dwim-global-set-key '("{" "(" "\"" "'" "`"))
+  (skeleton-pair-dwim-global-set-key
+   (skeleton-pair-dwim-default-pair-to-keys)
+   'skeleton-pair-insert-dwim-earmuff)
 
-  ;; (skeleton-pair-dwim-local-set-key
-  ;;  (skeleton-pair-dwim-default-alist-to-keys))
+  (setq skeleton-pair-alist '((?' _ ?') ) )
+  ;; (skeleton-pair-dwim-global-set-key
+  ;;  (skeleton-pair-dwim-default-pair-to-keys)
+  ;;  'self-insert-command)
+
+  (skeleton-pair-dwim-global-set-key skeleton-pair-dwim-default-keys)
+  ;;(skeleton-pair-dwim-global-set-key '("{" "(" "\"" "'" "`" "[") 'self-insert-command)
+  ;;(skeleton-pair-dwim-local-set-key '("{" "(" "\"" "'" "`" "["))
   ;;(skeleton-pair-dwim-global-set-key '("{" "(" "\"" "'" "`") 'self-insert-command)
   ;;(skeleton-pair-dwim-global-set-key '("{" "(" "\"" "'" "`") nil)
   )
